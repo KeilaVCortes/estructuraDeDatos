ab6c717c8dab62135fdb381d726555a003c6ff20
margotduek1 <margotduek@gmail.com>
2015-01-19 20:32:07 +0000

    :see_no_evel: ejercicios de la segunda clase, no compila

--- 1recursividad.cpp
+++ 1recursividad.cpp
@@ -7,18 +7,25 @@ Ejercicio recursividad
 using namespace std;
 
 int euclides(int m, int n);
-int vector(int a, int b, int c);
+int vector(int arr[], int i, int n);
+int suma(int fila, int columna, int mat[m][n]);
 
 int main(){
     int a = 0;
     int b = 0;
     int c = 0;
+    
+    int ar[8];
+    int matrix[][];
     cin >> a;
     cin >> b;
     cin >> c;
+    for(int i = 0; i < 8 ; i++){
+        ar[i] = 1 + i;
+    }
     cout << euclides(a,b) << endl;
-    cout << vector(a, b, c) << endl;
-    
+    cout << vector(ar, 0, 8) << endl;
+    cout << suma(5,5,matrix) << endl;
     
     return 0;    
 }
@@ -41,7 +48,18 @@ int vector(int arr[], int i, int n){
     if (i == n){
         return 0;
     }else{
-        return arr[i] + suma(arr, (i+1), r);
+        return arr[i] + vector(arr, (i+1), n);
     }
 }
 
+//Ejercicio 3
+//Suma de elemnetos de una matriz
+int suma(int fila, int columna, int mat[m][n]){
+    if(fila == 0 && columna ==0){
+        return mat[fila][columna];
+    }else if (fila == 0){
+        return mat[fila][columna] + suma(mat,fila-1, m-1);
+    }else{
+        return mat[fila][columna] + suma(mat, fila-1, columna-1);
+    }
+}
