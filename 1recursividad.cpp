9b6e385e9504813d2e22cbc90c5e21bc878cfa19
margotduek1 <margotduek@gmail.com>
2015-01-19 22:38:02 +0000

    :smile: funcion de Euclides y suma de vectores recursivas

--- 1recursividad.cpp
+++ 1recursividad.cpp
@@ -6,27 +6,29 @@ Ejercicio recursividad
 #include <string>
 using namespace std;
 
+    
 int euclides(int m, int n);
 int vector(int arr[], int i, int n);
-int suma(int fila, int columna, int mat[m][n]);
+
+
 
 int main(){
     int a = 0;
     int b = 0;
     int c = 0;
-    
+
     int ar[8];
-    int matrix[][];
     cin >> a;
     cin >> b;
     cin >> c;
     for(int i = 0; i < 8 ; i++){
         ar[i] = 1 + i;
     }
-    cout << euclides(a,b) << endl;
-    cout << vector(ar, 0, 8) << endl;
-    cout << suma(5,5,matrix) << endl;
+
     
+    cout << "el maximo comun divisor de " << a << " y " << b <<" es : " << euclides(a,b) << endl;
+    cout << "la suma de los vectores es : " << vector(ar, 0, 8) << endl;
+
     return 0;    
 }
 
@@ -49,17 +51,5 @@ int vector(int arr[], int i, int n){
         return 0;
     }else{
         return arr[i] + vector(arr, (i+1), n);
-    }
-}
-
-//Ejercicio 3
-//Suma de elemnetos de una matriz
-int suma(int fila, int columna, int mat[m][n]){
-    if(fila == 0 && columna ==0){
-        return mat[fila][columna];
-    }else if (fila == 0){
-        return mat[fila][columna] + suma(mat,fila-1, m-1);
-    }else{
-        return mat[fila][columna] + suma(mat, fila-1, columna-1);
     }
 }
