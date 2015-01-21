93407aaeacc5f8a2c82a25f292d979c4ea7fd5eb
margotduek1 <margotduek@gmail.com>
2015-01-21 05:26:08 +0000

    :seenoevil: ya estan los algoritmos, solo falta revisar bien el 6

--- 1.3recursividad.cpp
+++ 1.3recursividad.cpp
@@ -11,17 +11,22 @@ using namespace std;
 
 int sumaDigitos(int numero);
 string invertir(string numero, int tam);
+string palindromo(string palabra, int i, int tama);
 
 int main(){
     int n;
     string nu;
+    string palabra;
     
     cout << "pon un numero" << endl;
     cin >> n;
     cin >> nu;
+    cin >> palabra;
+    int tama = palabra.length();
     int tam = nu.length();
     cout << sumaDigitos(n) << endl;
     cout << invertir(nu, tam+1 ) << endl;
+    cout << palindromo(palabra,1,tama) << endl;
     return 0;
 }
 
@@ -50,3 +55,20 @@ string invertir(string numero, int tam){
         return inverso += invertir(numero, tam-1);
     }
 }
+
+
+//Ejercicio 6
+//Revisa si una palabra es palindromo
+string palindromo(string palabra, int i, int tama){
+    
+    if(tama == 2){
+        return "es un bifonte";
+    }else{
+        if(palabra[i] == palabra[tama]){
+            return palindromo(palabra, i+1, tama-1);
+        }else{
+            return "no es palindromo";
+        }
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # estructuraDeDatos
 clase de estructura de datos en c++ 
+
+En esste repositorio se encuentran ejercicios sobre:
+    - funciones recursivas
