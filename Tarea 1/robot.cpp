8c97884a8e0ed9155859d80825715fd6ca19b7fd
margotduek1 <margotduek@gmail.com>
2015-01-30 06:19:41 +0000

    :smile: tarea 1

--- Tarea 1/robot.cpp
+++ Tarea 1/robot.cpp
@@ -7,8 +7,8 @@ Algoritmo de una funcion recursiva que permita a un robot moverse.
 #include<iostream>
 #include<string>
 #include<cstdlib>
-#define n 7
-#define m 10
+#define n 5
+#define m 5
 
 using namespace std;
 
@@ -17,35 +17,32 @@ void imprimeMatriz(int mat[n][m]);
 
 int main(){
     int mat[n][m];
+    int cont = 1;
    
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < m; ++j) {
-            mat[i][j] = rand() % 9;
+            mat[i][j] = rand() % 1;
         }
     }
     
-    buscaCamino(mat,0,0,1);
-    
-
+    imprimeMatriz(mat);
+    buscaCamino(mat,0,0,cont);
     return 0;
 }
 
 
-int ajedrez(int mat[n][m], int i, int j, int cont){
-    if(i < n || j < m ){
-        return(mat, i+3, j+2,cont+1);
-        cout << cont;
-    }
-}
+
 
 void buscaCamino(int mat[n][m], int i, int j, int cont){
     if(mat[i][j] == 0 && i < n && j < n){
         mat[i][j] = 3;
-        if(mat[n-1][m-1] == 3){
+        
+        buscaCamino(mat, i, j+3, cont);
+        buscaCamino(mat, i+1,j,cont);
+    
+        if(mat[n][m] == 3){
             cont++;
         }
-        buscaCamino(mat, i, j+3, cont);
-        buscaCamino(mat, i+1,2,cont);
     }
     cout << cont << endl;
 }
