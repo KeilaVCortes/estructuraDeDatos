c02aa63dfa82b179ce6dc8efee7deeb7e8c7e86b
margotduek <margotduek@gmail.com>
2015-05-12 14:23:43 -0500

    Examen final

--- Ultimo Parcial/ExamenFinal/recursividad.cpp
+++ Ultimo Parcial/ExamenFinal/recursividad.cpp
@@ -8,26 +8,48 @@
 #include <iostream>
 #define n 6
 #define m 6
+using namespace std;
+
+void fractales(int mat[n][m], int i, int j, int nn);
 
 int main(){
   int mat[n][m];
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < m; ++j) {
-      mat[i][j] = 0;
+      mat[i][j] = 1;
     }
   }
+  for (int i = 2; i <= 3; ++i) {
+    for (int j = 2; j <= 3; ++j) {
+      mat[i][j] = 8;
+    }
+  }
+
+  fractales(mat, 0, 0, 5);
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < m; ++j) {
+      cout << mat[i][j];
+    }
+    cout << endl;
+  }
+
+  //fractales(mat, 0, 0, 5);
 
   return 0;
 }
 
-
-
-
-void buscaCamino2(int mat[n][m], int i, int j){
-    if(mat[i][j] == 0 && i < n && j < n){
-        mat[i][j] = 3;
-        buscaCamino2(mat, i, j+1);
-        buscaCamino2(mat, i+1, j);
-    }
+void fractales(int mat[n][m], int i, int j, int nn){
+  if(mat[i][j] == 8 && i < n && j < n){
+      mat[i-1][j-1] = 8;
+      mat[i+2][j-1] = 8;
+      mat[i-1][j+2] = 8;
+      mat[i+2][j+2] = 8;
+      //fractales(mat, 0, 0, nn-1);
+  }
+  else if(mat[i][j] == 1 && i < n && j < n){
+    fractales(mat, i+1, j+1, nn);
+    //fractales(mat, i+1, j, nn);
+  }
 }
